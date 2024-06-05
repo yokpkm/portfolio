b56a0ca8ecd0fea19bdcd9d97fa4add9ac67fa07
yokpkm <yok.pkm@gmail.com>
2024-06-05 19:53:58 +0700

    💄 adjust card component

--- src/app/components/card/index.tsx
+++ src/app/components/card/index.tsx
@@ -1,19 +1,19 @@
-import { Card } from "antd";
 import styled from "styled-components";
-import { PropsTheme } from "../../../theme";
+import { PropsTheme } from "@/theme";
+import { Card } from "antd";
 
-export const StyledCard: any = styled(Card)`
-  &&& .ant-card {
-    border-radius: 8px;
-    border: 1px solid ${(props: PropsTheme) => props.theme.lineColor.default};
+export const StyledCard = styled(Card)<PropsTheme>`
+  &&.ant-card {
+    background: ${(props) => props.theme.backgroundColors.card};
+    border-radius: 16px !important;
+    box-shadow: ${(props) => props.theme.cardShadow.default};
   }
 
-  .ant-card-cover img {
-    border-radius: 0px !important;
+  &&.ant-card-bordered {
+    border: 0px solid ${(props) => props.theme.backgroundColors.card};
   }
 
-  .ant-card-body {
-    background: ${(props: PropsTheme) => props.theme.backgroundColors.default};
-    padding: 32px 0px 24px 0px;
+  &&.ant-card-body {
+    padding: 32px 24px 24px 24px;
   }
 `;
