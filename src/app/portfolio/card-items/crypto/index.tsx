bc24e015785ea549de9e4c6bf4c07b28df6df7ac
yokpkm <yok.pkm@gmail.com>
2025-06-15 04:29:04 +0700

    🧪 optimize image quality

--- src/app/portfolio/card-items/crypto/index.tsx
+++ src/app/portfolio/card-items/crypto/index.tsx
@@ -1,4 +1,5 @@
-import { Image } from "@heroui/react";
+// import { Image } from "@heroui/react";
+import Image from "next/image";
 import { Row, Col } from "antd";
 import { ModalCrypto } from "./components/modal";
 import { StyledCard } from "@/app/components/card";
@@ -15,18 +16,40 @@ export const CardCrypto: React.FC<Props> = ({ open, setOpen }) => {
       <ModalCrypto open={open} setOpen={setOpen} />
       <Col xs={24} sm={12} md={12} lg={12} xl={12} xxl={12}>
         <StyledCard
-          cover={
-            <Image
-              src="/image/card/crypto.svg"
-              alt=""
-              width="100%"
-              style={{ borderRadius: 16 }}
-            />
-          }
+          // cover={
+          //   // <Image
+          //   //   src="/image/card/crypto.svg"
+          //   //   alt=""
+          //   //   width="100%"
+          //   //   style={{ borderRadius: 16 }}
+          //   // />
+          // }
           hoverable
           onClick={() => setOpen(true)}
         >
           <Row gutter={[0, 4]} style={{ marginTop: 12 }}>
+            <Col span={24}>
+              <div
+                style={{
+                  position: "relative",
+                  width: "100%",
+                  aspectRatio: "1 / 1",
+                  marginBottom: 32,
+                }}
+              >
+                <Image
+                  src="/image/card/crypto.svg"
+                  alt=""
+                  quality={100}
+                  priority
+                  fill
+                  style={{
+                    borderRadius: 16,
+                    objectFit: "cover",
+                  }}
+                />
+              </div>
+            </Col>
             <Col span={24}>
               <TextCardTitle>Cryptocurrecy Application</TextCardTitle>
             </Col>
