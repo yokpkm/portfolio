d84156e8bb3a3d1a3e548165a73f55afe6e9c048
yokpkm <yok.pkm@gmail.com>
2025-06-20 20:11:48 +0700

    🧪 change close icon

--- src/app/portfolio/card-items/crypto/components/modal.tsx
+++ src/app/portfolio/card-items/crypto/components/modal.tsx
@@ -58,7 +58,6 @@ export const ModalCrypto: React.FC<Props> = ({ open, setOpen }) => {
         //     />
         //   )
         // }
-        closeIcon={showCloseIcon}
         styles={{ mask: { backgroundColor: "rgba(0, 0, 0, 0.8)" } }}
       >
         <motion.div
